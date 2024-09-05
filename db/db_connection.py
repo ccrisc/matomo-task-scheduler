cab8b28d6cbf8816dcd7d3824bf037457876b596
ccriscione <cesarina.criscione@gmail.com>
2024-09-06 01:24:23 +0200

    Fix

--- db/db_connection.py
+++ db/db_connection.py
@@ -8,7 +8,7 @@ def get_connection():
         conn = psycopg2.connect(
             host=os.getenv('SUPABASE_HOST'),
             port=os.getenv('SUPABASE_PORT'),
-            dbname=os.getenv('SUPABASE_DB'),
+            dbname=os.getenv('SUPABASE_DB_NAME'),
             user=os.getenv('SUPABASE_USER'),
             password=os.getenv('SUPABASE_PASSWORD')
         )
